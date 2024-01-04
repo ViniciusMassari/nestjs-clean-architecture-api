73dc7e1cab7603c349b742139c5094e6c5d9652e
Vinícius Massari <stonefields11@hotmail.com>
2024-01-04 20:48:49 -0300

    Adding UserDataBuilder helper method

--- src/users/domain/entities/__tests__/unity/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unity/user.entity.spec.ts
@@ -1,15 +1,11 @@
-import { faker } from '@faker-js/faker'
+import { UserDataBuilder } from '../../../testing/helpers/user-data-builder'
 import { UserEntity, UserProps } from '../../user.entity'
 
 describe('UserEntity unit tests', () => {
   let props: UserProps
   let sut: UserEntity
   beforeEach(() => {
-    props = {
-      name: faker.person.firstName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    }
+    props = UserDataBuilder({})
 
     sut = new UserEntity(props)
   })
