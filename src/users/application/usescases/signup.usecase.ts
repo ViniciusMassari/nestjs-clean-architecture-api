3ca755b38d1bbb93075dd281520b5021cfcab12e
Vinícius Massari <stonefields11@hotmail.com>
2024-01-15 20:19:17 -0300

    Adding usecase SignUpUseCase - part 2

--- src/users/application/usescases/signup.usecase.ts
+++ src/users/application/usescases/signup.usecase.ts
@@ -1,3 +1,7 @@
+import { UserRepository } from '@/users/domain/repositories/user.repository'
+import { BadRequestError } from '../errors/bad-request-error'
+import { UserEntity } from '@/users/domain/entities/user.entity'
+
 export namespace SignupUseCase {
   // parametros de entrada
   export interface Input {
@@ -16,7 +20,20 @@ export namespace SignupUseCase {
   }
 
   export class UseCase {
+    // construstor para injeção de dependência
+    constructor(private userRepository: UserRepository.Repository) {}
     // o método pode ser chamado de handle ou execute
-    async execute(input: Input): Promise<Output> {}
+    async execute(input: Input): Promise<Output> {
+      const { email, name, password } = input
+      if (!email || !name || !password) {
+        throw new BadRequestError('Input data not provided')
+      }
+      await this.userRepository.emailExists(email)
+
+      const entity = new UserEntity(input)
+      await this.userRepository.insert(entity)
+
+      return entity.toJSON()
+    }
   }
 }
